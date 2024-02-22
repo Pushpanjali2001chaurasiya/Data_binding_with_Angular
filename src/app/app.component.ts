6c627f35f3e8ceec7cd7df6bfc1b8621264ee8b8
Pushpanjali <chaurasiyapushpanjali256@gmail.com>
2024-02-22 17:19:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'databinding';
+  blogtitle:string="";
+  blogdetails:string="";
+  imageURL:string="";
+  blogURL:string="";
+  
 }
